8cb101b396c8715aeefa73cd32cd1b81b05ead43
Rooneydav <onyanchadvd@gmail.com>
2023-07-24 13:08:35 -0700

    changes

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -6,13 +6,16 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    char *str;
-    int len;
+char *str;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+int len;
+
+str = "My first strlen!";
+
+len = _strlen(str);
+printf("%d\n", len);
+return (0);
 }
